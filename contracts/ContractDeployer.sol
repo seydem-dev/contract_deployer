f9aed3ff40616616c9e5d936b7d0d468a6954baa
Seyon Demiroglu <seyon.demiroglu01@gmail.com>
2022-06-30 22:03:21 +0200

    Remove functions and mappings

--- contracts/ContractDeployer.sol
+++ contracts/ContractDeployer.sol
@@ -18,7 +18,6 @@ contract ContractDeployer {
     mapping(address => address) public getLatestContractAddressOfOwner;
     mapping(address => address) public getContractOwner;
     mapping(address => uint8) private _deployedContractsPerWallet;
-    mapping(address => address[]) private _trackingContracts;
 
     struct Addresses {
         address contractOwner;
@@ -32,7 +31,7 @@ contract ContractDeployer {
     function deployContract(uint8 contractsAmount) external payable {
         require(tx.origin == msg.sender, "The function caller is not an Externally Owned Account");
         uint8 maxContractsPerTx = 5;
-        uint8 maxContractsPerWallet = 20;
+        uint8 maxContractsPerWallet = 50;
         uint56 deploymentPrice = 0.005 ether;
         require(contractsAmount != 0 && contractsAmount <= maxContractsPerTx, "You can not deploy more than 5 contracts per transaction");
         require(msg.value == contractsAmount * deploymentPrice, "You need to pay 0.005 ETH per contract");
@@ -41,7 +40,6 @@ contract ContractDeployer {
             Contract deployedContract = new Contract();
             getContractWithOwnerAddress.push(Addresses({contractOwner: msg.sender, contractAddress: address(deployedContract)}));
             contracts.push(address(deployedContract));
-            _trackingContracts[msg.sender] = contracts;
             getLatestContractAddressOfOwner[msg.sender] = address(deployedContract);
             getContractOwner[address(deployedContract)] = msg.sender;
             unchecked {
@@ -65,12 +63,5 @@ contract ContractDeployer {
     function withdraw() external {
         require(msg.sender == owner, "You are not the owner of this contract");
         payable(owner).transfer(address(this).balance);
-    }
-
-    /**
-      * @notice Function returns balance of this smart contract in GWEI, 1e9 GWEI = 1 ETH
-      */
-    function balanceOfContractInGwei() external view returns (uint256) {
-        return address(this).balance / 1e9;
     }
 }
