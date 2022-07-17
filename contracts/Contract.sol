74c39a901443fd994170128ccb43f95dc5a55f11
Seyon Demiroglu <seyon.demiroglu01@gmail.com>
2022-07-17 13:28:28 +0200

    Reduce code

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -1,6 +1,6 @@
 contract Contract {
 
     function sample() external pure returns (string memory) {
-        return "This is a sample contract. Let your creativity guide you and see what this contract could be :) ";
+        return "This is a sample contract";
     }
 }
